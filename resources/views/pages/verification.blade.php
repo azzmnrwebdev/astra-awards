fdbd686e22bd813bfdaf5131dbcfb3e11e2b0a4a
Hadi Ariwibowo <hadi.ariwibowo@gmail.com>
2024-09-18 22:02:05 +0700

    Perubahan kalimat sapa sesuai dengan poin 16 dokumen Revisi 18 Sept 2024 AAA.docx.

--- resources/views/pages/verification.blade.php
+++ resources/views/pages/verification.blade.php
@@ -2,11 +2,7 @@
     <div class="container py-4">
         <div class="alert alert-success" role="alert">
             <h5 class="alert-heading">Terima kasih telah menyelesaikan registrasi Anda.</h5>
-            <p>Proses verifikasi akun Anda sedang berlangsung. Mohon tunggu, karena proses ini memerlukan waktu paling
-                cepat 1 jam dan paling lambat 1 hari untuk diselesaikan oleh panitia. Kami akan mengirim ke email Anda
-                setelah proses verifikasi selesai.</p>
-            <hr>
-            <p class="mb-0">Semoga harimu menyenangkan</p>
+            <p>Panitia akan memverifikasi akun Anda untuk dapat melanjutkan ke proses pengisian form lima pilar. Kami akan mengirim ke email Anda setelah proses verifikasi selesai. Terima kasih.</p>
         </div>
     </div>
 </x-user>
